ebc1fe84c6f537075ca9be647be574dc0d19b425
田原 一樹 <ssp0727.lnc@gmail.com>
2021-08-22 19:29:14 +0900

    update

--- app.ts
+++ app.ts
@@ -1,13 +1,21 @@
+import fs from 'fs'
 import express from 'express'
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.status(200).send('hello from the server side')
+const tours = JSON.parse(
+  `${fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)}`
+)
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: { tours },
+  })
 })
 
 const port = 3000
 
 app.listen(port, () => {
-  console.info('app aarunning')
+  console.info('app running')
 })
