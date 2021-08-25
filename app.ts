e88b620b2305ad72c202e8779a884f3a49599ef4
田原 一樹 <ssp0727.lnc@gmail.com>
2021-08-25 21:31:09 +0900

    update

--- app.ts
+++ app.ts
@@ -135,11 +135,17 @@ const deleteUser = (_req: Request, res: Response) => {
   })
 }
 
-app.route('/api/v1/tours').get(getAllTours).post(createTour)
-app.route('/api/v1/tours/:id').get(getTour).patch(updateTour).delete(deleteTour)
+const tourRouter = express.Router()
+const userRouter = express.Router()
 
-app.route('/api/v1/users').get(getAllUsers).post(createUsers)
-app.route('/api/v1/users/:id').get(getUser).patch(updateUser).delete(deleteUser)
+tourRouter.route('/').get(getAllTours).post(createTour)
+tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour)
+
+userRouter.route('/').get(getAllUsers).post(createUsers)
+userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser)
+
+app.use('/api/v1/tours', tourRouter)
+app.use('/api/v1/users', userRouter)
 
 const PORT = 3000
 
