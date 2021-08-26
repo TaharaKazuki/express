e153706772a69bc8184151249f7d53f261af30a0
田原 一樹 <ssp0727.lnc@gmail.com>
2021-08-26 22:08:58 +0900

    update

--- routes/tourRoutes.ts
+++ routes/tourRoutes.ts
@@ -6,12 +6,13 @@ import {
   updateTour,
   deleteTour,
   checkID,
+  checkBody,
 } from './../controllers/tourController'
 
 const router = Router()
 
 router.param('id', checkID)
-router.route('/').get(getAllTours).post(createTour)
+router.route('/').get(getAllTours).post(checkBody, createTour)
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour)
 
 export default router
