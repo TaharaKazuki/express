f97edc785d5cadb8ec7b3412fae183c79f2eb0a5
田原 一樹 <kazuki.tahara@rarejob.co.jp>
2021-08-26 10:56:33 +0900

    update

--- controllers/tourController.ts
+++ controllers/tourController.ts
@@ -1,12 +1,13 @@
 import { Request, Response, NextFunction } from 'express'
 import fs from 'fs'
 
+type ResponseStatus = Response<any, Record<string, any>> | undefined
 
 const tours = JSON.parse(
   `${fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)}`
 )
 
-export const checkID = (req:Request, res: Response, next: NextFunction) => {
+export const checkID = (req:Request, res: Response, next: NextFunction) : ResponseStatus => {
   if(+req.params.id > tours.length) {
     return res.status(404).json({
       status: 'fail',
